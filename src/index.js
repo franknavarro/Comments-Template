53e013f570e5111216749e7040826da76bf8169f
Frank Navarro <fnavarro7777@gmail.com>
2019-05-29 06:59:37 -0700

    Approval card now takes children props

--- src/index.js
+++ src/index.js
@@ -7,25 +7,36 @@ import ApprovalCard from './ApprovalCard';
 const App = () => {
   return (
     <div className="ui container comments">
-      <ApprovalCard />
-      <CommentDetail 
-        author="Sam" 
-        datePosted="Today at 3:00PM" 
-        avatar={faker.image.avatar()} 
-        text="Nice blog post!" 
-      />
-      <CommentDetail 
-        author="Frank" 
-        datePosted="Yesterday at 9:00AM" 
-        avatar={faker.image.avatar()} 
-        text="Nice blog post!" 
-      />
-      <CommentDetail 
-        author="Rodrigo" 
-        datePosted="Yesterday at 1:00AM" 
-        avatar={faker.image.avatar()} 
-        text="Nice blog post!" 
-      />
+      <ApprovalCard>
+        <div>
+          <h4>Warning</h4>
+          Are you sure you want to do this?
+        </div>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail 
+          author="Sam" 
+          datePosted="Today at 3:00PM" 
+          avatar={faker.image.avatar()} 
+          text="Nice blog post!" 
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail 
+          author="Frank" 
+          datePosted="Yesterday at 9:00AM" 
+          avatar={faker.image.avatar()} 
+          text="Nice blog post!" 
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail 
+          author="Rodrigo" 
+          datePosted="Yesterday at 1:00AM" 
+          avatar={faker.image.avatar()} 
+          text="Nice blog post!" 
+        />
+      </ApprovalCard>
     </div>
   );
 };
